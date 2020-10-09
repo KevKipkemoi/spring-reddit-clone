392fe876c7345de4b449d0ed27f9a4685b5bb583
KevKipkemoi <kevkipkemoi@outlook.com>
2020-10-09 19:43:27 +0300

    repository

---
+++ src/main/java/com/reddit/springredditclone/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.reddit.springredditclone.repository;
+
+import com.reddit.springredditclone.model.Comment;
+import com.reddit.springredditclone.model.Post;
+import com.reddit.springredditclone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+    List<Comment> findAllByUser(User user);
+}
---
+++ src/main/java/com/reddit/springredditclone/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.reddit.springredditclone.repository;
+
+import com.reddit.springredditclone.model.Post;
+import com.reddit.springredditclone.model.Subreddit;
+import com.reddit.springredditclone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllBySubreddit(Subreddit subreddit);
+    List<Post> findByUser(User user);
+}
