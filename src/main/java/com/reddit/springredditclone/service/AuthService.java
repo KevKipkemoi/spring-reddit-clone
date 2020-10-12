6d7e03a173ac1ce1e28d3dacecda833894478d39
KevKipkemoi <kevkipkemoi@outlook.com>
2020-10-12 20:48:58 +0300

    controller, dto and service

---
+++ src/main/java/com/reddit/springredditclone/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.reddit.springredditclone.service;
+
+import com.reddit.springredditclone.dto.RegisterRequest;
+import com.reddit.springredditclone.model.User;
+import com.reddit.springredditclone.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static java.time.Instant.now;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUserName(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
+        user.setCreated(now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+}
