0ae10b85f1ba335497e70a49c8578bb0615187b3
KevKipkemoi <kevkipkemoi@outlook.com>
2020-10-19 09:52:34 +0300

    login, refresh-token and logout mappings

--- src/main/java/com/reddit/springredditclone/controller/AuthController.java
+++ src/main/java/com/reddit/springredditclone/controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.reddit.springredditclone.controller;
 
+import com.reddit.springredditclone.dto.AuthenticationResponse;
+import com.reddit.springredditclone.dto.LoginRequest;
+import com.reddit.springredditclone.dto.RefreshTokenRequest;
 import com.reddit.springredditclone.dto.RegisterRequest;
 import com.reddit.springredditclone.service.AuthService;
+import com.reddit.springredditclone.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -13,6 +19,7 @@ import static org.springframework.http.HttpStatus.OK;
 @AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
@@ -24,5 +31,21 @@ public class AuthController {
     public ResponseEntity<String> verifyAccount(@PathVariable String token) {
         authService.verifyAccount(token);
         return new ResponseEntity<>("Account activated successfully", OK);
+    }
+
+    @PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+
+    @PostMapping("refresh/token")
+    public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return ResponseEntity.status(OK).body("Refresh token deleted successfully");
     }
 }
