5aea1842a49fb954aaf32df8ff28e01f439b3c51
KevKipkemoi <kevkipkemoi@outlook.com>
2020-10-28 23:58:16 +0300

    resource handler

--- src/main/java/com/reddit/springredditclone/config/WebConfig.java
+++ src/main/java/com/reddit/springredditclone/config/WebConfig.java
@@ -19,4 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .exposedHeaders("Authorization")
                 .allowCredentials(true);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
